6b4b9f195f48a2f34b9bc568006a476ad9b7733f
eman <ealdadah@gmail.com>
2017-12-23 16:03:51 +0200

    add models to admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+
+# Register your models here
+from shop.models import Category, Product, Order
+
+#
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ['cost', 'category']
+#
+#
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = ['total_cost ', 'created_at', 'delivery_date']
+
+
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Product)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    cost = models.IntegerField()
+    category = models.ManyToManyField(Category, related_name="products")
+    numberOfProduct = models.IntegerField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    product = models.ManyToManyField(Product, related_name="orders")
+    total_cost = models.IntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
+    delivery_date = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return "%s buy some products" % (self.user.username)
